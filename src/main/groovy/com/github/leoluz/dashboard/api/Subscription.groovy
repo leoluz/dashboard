f8e22cd3a0a6b6f22709cdafbe5e2cb3e2ddef4e
Leonardo Luz Almeida <leonardo.la@gmail.com>
2016-04-09 15:47:15 -0400

    add spring security

--- src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
+++ src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
@@ -5,13 +5,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET
 
 @RestController
 @RequestMapping("/api/subscriptions")
@@ -19,14 +16,10 @@ class Subscription {
 
 	private static final Logger logger = LoggerFactory.getLogger(Subscription.class)
 
-	@RequestMapping(value="/hi/{name}", method=GET)
-	String home(@PathVariable String name) {
-		logger.info ">> name: ${name}"
-		"Hello! My name is ${name}"
-	}
+	@RequestMapping(value = "/create")
+	ResponseEntity<?> create(@RequestParam("url") String url,
+							 @RequestHeader HttpHeaders headers) {
 
-	@RequestMapping(value="/create")
-	ResponseEntity<?> create(@RequestParam("url") String url, @RequestHeader HttpHeaders headers) {
 		headers.toSingleValueMap().each { key, value ->
 			logger.info ">> ${key}: ${value}"
 		}
