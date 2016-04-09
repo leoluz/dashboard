229c08118c98a563cf1375edaa406e7b8883ccbf
Leonardo Luz Almeida <leonardo.la@gmail.com>
2016-04-08 22:37:29 -0400

    add logback configuration

--- src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
+++ src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
@@ -1,5 +1,7 @@
 package com.github.leoluz.dashboard.api
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,11 +9,20 @@ import org.springframework.web.bind.annotation.RestController
 import static org.springframework.web.bind.annotation.RequestMethod.GET
 
 @RestController
-@RequestMapping("/api")
-class DashboardApi {
+@RequestMapping("/api/subscriptions")
+class Subscription {
+
+	private static final Logger logger = LoggerFactory.getLogger(Subscription.class)
 
 	@RequestMapping(value="/hi/{name}", method=GET)
 	String home(@PathVariable String name) {
+		logger.info ">> name: ${name}"
 		"Hello! My name is ${name}"
+	}
+
+	@RequestMapping(value="/create/{token}")
+	String create(@PathVariable String token) {
+		logger.info ">> token: ${token}"
+		token
 	}
 }
