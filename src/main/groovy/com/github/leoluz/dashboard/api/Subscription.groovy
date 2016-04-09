6e3bb71c11957048ce94d2f56f61527a48117a92
Leonardo Luz Almeida <leonardo.la@gmail.com>
2016-04-09 12:04:10 -0400

    log all request headers

--- src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
+++ src/main/groovy/com/github/leoluz/dashboard/api/Subscription.groovy
@@ -2,9 +2,11 @@ package com.github.leoluz.dashboard.api
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -24,8 +26,10 @@ class Subscription {
 	}
 
 	@RequestMapping(value="/create")
-	ResponseEntity<?> create(@RequestParam("url") String url) {
-		logger.info ">> url: ${url}"
+	ResponseEntity<?> create(@RequestParam("url") String url, @RequestHeader HttpHeaders headers) {
+		headers.toSingleValueMap().each { key, value ->
+			logger.info ">> ${key}: ${value}"
+		}
 		def responseBody = [ success: true,
 							 accountIdentifier: "new-account-identifier" ]
 		new ResponseEntity<>(responseBody, HttpStatus.CREATED)
